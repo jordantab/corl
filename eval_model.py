64ff84cea5e14ad86b8fe6ef778b55e5a00d54a9
jordantab <jordantab20@gmail.com>
2024-04-28 11:01:29 -0700

    Update eval_model.py

--- eval_model.py
+++ eval_model.py
@@ -78,7 +78,6 @@ def eval_model(checkpoint, dataset, device):
 
         # evaluate generated code
         if verdict == "Accepted":
-
             if runtime < runtime_slow:
                 outputs_slow.append(1)
             if runtime < runtime_fast:
@@ -107,13 +106,14 @@ def generate_code(checkpoint, slow_code, device):
         model_kwargs={"torch_dtype": torch.bfloat16},
         device_map=device,
     )
+    temp_code = 'def fibonacci_recursive(n): return 0 if n == 0 else 1 if n == 1 else fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2); print(f"The {n}th Fibonacci number (slow way): {fibonacci_recursive(10)}")'
 
     messages = [
         {
             "role": "system",
             "content": "Provide an optimized version of the following code snippet. Only provide the code, no need to provide any description. ",
         },
-        {"role": "user", "content": slow_code},
+        {"role": "user", "content": temp_code},
     ]
 
     prompt = pipeline.tokenizer.apply_chat_template(
