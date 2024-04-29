a01b8eabf26779a9767e3dfd6cf4b419bc4514be
Herbie Rand <hjrand1005@gmail.com>
2024-04-28 17:12:42 -0700

    Fix defaults in run_eval_model.sh, config
    Co-authored-by: Jordan Tab <jordantab20@gmail.com>
    Co-authored-by: Herbie Rand <hjrand1005@gmail.com>

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DEFAULT_TEST_DATASET_PATH = "./testset/python_leq_60_tokens_test.json"
 DEFAULT_OUTDIR = "models/dataset_tuned_checkpoint"
 
 # Data preprocessing
-DEFAULT_MAX_LEN = 25
+DEFAULT_MAX_LEN = 100
 DEFAULT_PADDING = "max_length"
 DEFAULT_TEST_SIZE = 1
 DEFAULT_INSTRUCTION_DATASET = "examples/sample1.json"
@@ -30,5 +30,5 @@ DEFAULT_DEEPSPEED = None
 DEFAULT_FP16 = False
 
 # Platform specific configs
-DEFAULT_DEVICE = "cpu"  # for GPU usage or "cpu" for CPU usage
+DEFAULT_DEVICE = "cuda"  # for GPU usage or "cuda" for CPU usage
 DEFAULT_NUM_PROCS = os.cpu_count()
--- run_eval_model.sh
+++ run_eval_model.sh
@@ -4,8 +4,8 @@
 # Usage: ./run_eval_model.sh <dataset_path> <device>
 
 # Assigning command line arguments to variables
-DATASET_PATH=${1:-"./examples/test_python.json"}  # Default path if not provided
-DEVICE=${2:-"cpu"}  # Default to "cpu" if not provided
+DATASET_PATH=${1:-"./testset/python_leq_60_tokens_test.json"}
+DEVICE=${2:-"cuda"}  # Default to "cuda" if not provided -- override with "cpu" locally
 
 # Running the Python script with parameters using 'time' for profiling
 echo "Running eval_model.py with dataset at '$DATASET_PATH' and on device '$DEVICE'"
